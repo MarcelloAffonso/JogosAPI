adcf6563f94c6dec428770218d1f880749b3aa6d
MarcelloAffonso <mars1803@gmail.com>
2022-12-28 20:27:24 -0300

    Add project files.

---
+++ JogosAPI/Controllers/JogoController.cs
@@ -0,0 +1,21 @@
+﻿using JogosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JogosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class JogoController : ControllerBase
+{
+    private static List<Jogo> jogos = new List<Jogo>();
+
+    [HttpPost]
+    public void AdicionaJogo([FromBody] Jogo jogo)
+    {
+        jogos.Add(jogo);
+        Console.WriteLine(jogo.Titulo);
+        Console.WriteLine(jogo.Descricao);
+        Console.WriteLine(jogo.Genero);
+        Console.WriteLine(jogo.DataLancamento.ToString());
+    }
+}
---
+++ JogosAPI/Models/Jogo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JogosAPI.Models;
+
+public class Jogo
+{
+    [Required(ErrorMessage ="O título do jogo é obrigatório!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do jogo é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O tamanho máximo do gênero é de 50 caracteres e não pode ser excedido!")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A data de lançamento do jogo é obrigatória!")]
+    public DateTime DataLancamento { get; set; }
+
+    [Required(ErrorMessage ="A descrição do jogo é obrigatória!")]
+    public string Descricao { get; set; }
+}
