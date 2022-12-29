8351185c40d9760d0abe309c11bce4918a9e9fe4
MarcelloAffonso <mars1803@gmail.com>
2022-12-29 20:08:08 -0300

    Capitulo 3 - Consultando e paginando

--- JogosAPI/Controllers/JogoController.cs
+++ JogosAPI/Controllers/JogoController.cs
@@ -1,5 +1,6 @@
 ﻿using JogosAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace JogosAPI.Controllers;
 
@@ -9,13 +10,33 @@ public class JogoController : ControllerBase
 {
     private static List<Jogo> jogos = new List<Jogo>();
 
+    private static int id = 0;
+
     [HttpPost]
-    public void AdicionaJogo([FromBody] Jogo jogo)
+    public IActionResult AdicionaJogo([FromBody] Jogo jogo)
     {
+        jogo.Id = id++;
         jogos.Add(jogo);
-        Console.WriteLine(jogo.Titulo);
-        Console.WriteLine(jogo.Descricao);
-        Console.WriteLine(jogo.Genero);
-        Console.WriteLine(jogo.DataLancamento.ToString());
+
+        // Por padrão, um post deverá gravar o objeto passado e depois devolve-lo para o cliente
+        return CreatedAtAction(nameof(RecuperaJogoPorId), new { id = jogo.Id },
+            jogo);
+    }
+
+    [HttpGet]
+    public IEnumerable<Jogo> RecuperaJogos([FromQuery]int skip = 0, [FromQuery]int take = 50)
+    {
+        // Passa os parametros skip e take para permitir a paginação da resposta
+        return jogos.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaJogoPorId(int id)
+    {
+        // Caso o jogo não seja encontrado, retorna um erro 404 (Not Found)
+        var jogo = jogos.FirstOrDefault(jogo => jogo.Id == id);
+        if (jogo == null) return NotFound();
+
+        return Ok(jogo);
     }
 }
--- JogosAPI/Models/Jogo.cs
+++ JogosAPI/Models/Jogo.cs
@@ -4,6 +4,8 @@ namespace JogosAPI.Models;
 
 public class Jogo
 {
+    public int Id { get; set; } 
+
     [Required(ErrorMessage ="O título do jogo é obrigatório!")]
     public string Titulo { get; set; }
 
